a20a1fd9211c2318257739c08c34ea9571426b52
KESTREL <kestrel.nine@gmail.com>
2016-09-23 01:47:14 +0900

    Day 2

---
+++ AndroidProgramming/app/src/main/java/com/ourincheon/androidprogramming/ch0104.java
@@ -0,0 +1,16 @@
+package com.ourincheon.androidprogramming;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by YANG-_-V on 2016-09-23.
+ */
+
+public class ch0104 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstamceState){
+        super.onCreate(savedInstamceState);
+
+    }
+}
